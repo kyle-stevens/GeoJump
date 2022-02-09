b870df469aace888f1752ddf8a5f8b2862d3a5f6
kyle-stevens <kylebstev@gmail.com>
2022-02-09 15:36:26 -0700

    things

--- Player_cube_demo.gd
+++ Player_cube_demo.gd
@@ -20,11 +20,12 @@ func _process(delta):
 	#if prev_position != position:
 	if abs(prev_position.x - position.x) > 0.09 or abs(prev_position.y - position.y) > 0.09:
 		is_jumping = true
+		pass
 	else:
 		is_jumping = false
 		#$Line2D.visible = true
 	prev_position = position
-	print(is_jumping, $Line2D.visible)
+	#print(is_jumping, $Line2D.visible)
 	iteration += 1
 	$Line2D.visible = false
 	
@@ -33,10 +34,32 @@ func _process(delta):
 		$Line2D.visible = true
 		pass
 		
-	if Input.is_action_just_released("ui_accept"):
+	if Input.is_action_just_released("ui_accept") and not is_jumping:
 		var offset = Vector2.ZERO
 		var target = get_global_mouse_position() - position
 		apply_impulse(offset, target*3)
 		$Line2D.visible = false
 
+
+func death():
+	pass
+	#respawn
+	var pos = get_parent().get_node("spawn_position")
+	print(pos)
+	
+	var scene = load("res://Player_cube_demo.tscn")
+	var instance = scene.instance()
+	instance.global_transform.origin = pos.global_transform.origin
+	get_parent().add_child(instance)
+	queue_free()
+	#position = pos.global_transform.origin
+	
 		
+
+
+
+
+
+func _on_Area2D_body_entered(body):
+	death()
+	print("dead")
