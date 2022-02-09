da6facda3bbfefb889c0f2cd21aa4145c6b526aa
kyle-stevens <kylebstev@gmail.com>
2022-02-09 08:41:33 -0700

    Fixed Line issue due to gravity and motion detection logic

--- Player_cube_demo.gd
+++ Player_cube_demo.gd
@@ -8,7 +8,7 @@ extends RigidBody2D
 var is_jumping = false
 var prev_position = position
 var iteration = 0
-var force = 50 
+
 var jumps = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,34 +17,26 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if prev_position != position:
+	#if prev_position != position:
+	if abs(prev_position.x - position.x) > 0.09 or abs(prev_position.y - position.y) > 0.09:
 		is_jumping = true
 	else:
 		is_jumping = false
+		#$Line2D.visible = true
 	prev_position = position
-	#print(is_jumping, position, iteration)
+	print(is_jumping, $Line2D.visible)
 	iteration += 1
 	$Line2D.visible = false
 	
 	$Line2D.set_point_position(0, get_transform().affine_inverse() * get_global_mouse_position())
 	if Input.is_action_pressed("ui_accept") and not is_jumping:
-		#print("jump")
-		#add_force(Vector2(0,0), Vector2(0,-500))
-		#apply_impulse(Vector2(0,0), Vector2(0,-500))
-		var offset = Vector2.ZERO
-		var target = get_global_mouse_position() - position
-		target = target.normalized()
-		if force <= 500:
-			force += 2
-		print(force)
 		$Line2D.visible = true
+		pass
 		
 	if Input.is_action_just_released("ui_accept"):
 		var offset = Vector2.ZERO
 		var target = get_global_mouse_position() - position
-		target = target.normalized()
-		apply_impulse(offset, target*force)
-		force = 50
+		apply_impulse(offset, target*3)
 		$Line2D.visible = false
 
 		
